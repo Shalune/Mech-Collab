8e0fa81e702d00bec08372777c931e2cd082ace2
Tyler <tylermain3049@gmail.com>
2018-07-13 12:51:45 -0400

    tweaked camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,13 +7,15 @@ public class CameraFollow : MonoBehaviour {
     //public Transform player;
     public float rotationCatchupSpeed;
     public float positionCatchupSpeed;
+    public float forwardOffset;
 
     public void UpdateCamera(Transform player)
     {
         transform.rotation = Quaternion.Lerp(transform.rotation, player.rotation, rotationCatchupSpeed);
-        Vector2 newPos = Vector2.Lerp(transform.position, player.position, positionCatchupSpeed);
-        Debug.Log("playerpos = " + player.position + "    mypos = " + transform.position);
 
+        Vector2 newPos = Vector2.Lerp(transform.position, player.position, positionCatchupSpeed);
+        newPos += (Vector2)player.up * forwardOffset;
+    
         transform.position = new Vector3(newPos.x, newPos.y, transform.position.z);
         Debug.Log(newPos);
     }
