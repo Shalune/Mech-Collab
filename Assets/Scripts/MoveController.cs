dec7380fc954c58157891563e36bae7e3aebda27
Tyler <tylermain3049@gmail.com>
2018-07-13 11:39:36 -0400

    new move testing scene

---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour {
+
+    public string horizontalAxisName = "Horizontal";
+    public string verticalAxisName = "Vertical";
+
+    public Rigidbody2D rigidBody;
+    public float turnAccel;
+    public float forwardAccel;
+    public float reverseAccel;
+    public float turnMax;
+    public float forwardMax;
+    public float reverseMax;
+
+	void Update () {
+		
+	}
+
+    private void GetInput()
+    {
+        float horizontal = Input.GetAxis(horizontalAxisName);
+        float vertical = Input.GetAxis(verticalAxisName);
+
+        //rigidBody.velocity.y = Mathf.Lerp(rigidBody.velocity.y, forwardMax, forwardAccel);
+        rigidBody.AddForce(new Vector2(horizontal, vertical));
+    }
+}
